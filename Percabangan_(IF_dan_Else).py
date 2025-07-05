434372076983f3afaf3b3d58202d0138f3e337ef
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2025-07-05 21:28:15 +0700

    Add files via upload

---
+++ Percabangan_(IF_dan_Else).py
@@ -0,0 +1,15 @@
+angka = 5
+if angka > 0:
+    print("Angka positif")
+elif angka < 0:
+    print("Angka negatif")
+else:
+    print("Angka nol")
+    
+angka = 7 
+if angka > 1 :
+    print ("Angka positif")
+elif angka > 0 :
+    print ("Angka Negatif")
+else :
+    print ("Angka nol")
